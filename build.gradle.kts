8531413fa58b4c8e9cd2cc573316601b48083879
Radek Micek <radek.micek@gmail.com>
2020-11-21 11:35:50 +0100

    Configure Bintray publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 plugins {
     kotlin("jvm") version "1.4.10"
     `maven-publish`
+    id("com.jfrog.bintray") version "1.8.5"
 }
 
 group = "cz.radekm.records"
@@ -46,4 +47,20 @@ publishing {
             from(components["java"])
         }
     }
+}
+
+bintray {
+    user = System.getenv("BINTRAY_USER")
+    key = System.getenv("BINTRAY_KEY")
+    pkg(delegateClosureOf<com.jfrog.bintray.gradle.BintrayExtension.PackageConfig> {
+        repo = "maven"
+        name = "anonymous-records-kotlin"
+        githubRepo = "radekm/anonymous-records-kotlin"
+        vcsUrl = "https://github.com/radekm/anonymous-records-kotlin"
+        description = "Anonymous and slightly extensible records for Kotlin"
+        setLabels("record", "anonymous-record", "extensible-record", "poly-map", "shapeless", "kotlin")
+        setLicenses("MIT")
+        desc = description
+    })
+    setPublications("maven")
 }
